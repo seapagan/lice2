f9de86d4091a36aa4b0cfc8002b6ad07f07bb21a
Grant Ramsay <seapagan@gmail.com>
2024-08-18 11:30:17 +0100

    fix tests
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lice/__init__.py
+++ lice/__init__.py
@@ -3,4 +3,5 @@ __version__ = "0.6"
 
 def main():
     from lice.core import main
+
     main()
--- lice/tests/test_lice.py
+++ lice/tests/test_lice.py
@@ -3,8 +3,13 @@ from io import StringIO
 
 import lice
 from lice.core import (
-    LICENSES, clean_path, extract_vars, generate_license,
-    load_file_template, load_package_template)
+    LICENSES,
+    clean_path,
+    extract_vars,
+    generate_license,
+    load_file_template,
+    load_package_template,
+)
 
 TEMPLATE_PATH = os.path.dirname(lice.__file__)
 
@@ -22,59 +27,48 @@ def test_file_template():
             content = infile.read()
             assert content == load_file_template(path).getvalue()
 
+
 def test_package_template():
     for license in LICENSES:
         path = os.path.join(TEMPLATE_PATH, "template-%s.txt" % license)
         with open(path) as infile:
             assert infile.read() == load_package_template(license).getvalue()
 
+
 def test_extract_vars():
     template = StringIO()
     for license in LICENSES:
-        template.write(u'Oh hey, {{ this }} is a {{ template }} test.')
+        template.write("Oh hey, {{ this }} is a {{ template }} test.")
         var_list = extract_vars(template)
         assert var_list == ["template", "this"]
 
-def test_license():
 
-    context = {
-        "year": u'1981',
-        "project": u'lice',
-        "organization": u'Awesome Co.'
-    }
+def test_license():
+    context = {"year": "1981", "project": "lice", "organization": "Awesome Co."}
 
     for license in LICENSES:
-
         template = load_package_template(license)
         content = template.getvalue()
 
-        content = content.replace(u'{{ year }}', context["year"])
-        content = content.replace(u'{{ project }}', context["project"])
-        content = content.replace(u'{{ organization }}',
-                                    context["organization"])
+        content = content.replace("{{ year }}", context["year"])
+        content = content.replace("{{ project }}", context["project"])
+        content = content.replace("{{ organization }}", context["organization"])
 
         assert content == generate_license(template, context).getvalue()
         template.close()  # discard memory
 
-def test_license_header():
 
-    context = {
-        "year": u'1981',
-        "project": u'lice',
-        "organization": u'Awesome Co.'
-    }
+def test_license_header():
+    context = {"year": "1981", "project": "lice", "organization": "Awesome Co."}
 
     for license in LICENSES:
-
         try:
-
             template = load_package_template(license, header=True)
             content = template.getvalue()
 
-            content = content.replace(u'{{ year }}', context["year"])
-            content = content.replace(u'{{ project }}', context["project"])
-            content = content.replace(u'{{ organization }}',
-                                        context["organization"])
+            content = content.replace("{{ year }}", context["year"])
+            content = content.replace("{{ project }}", context["project"])
+            content = content.replace("{{ organization }}", context["organization"])
 
             assert content == generate_license(template, context).getvalue()
             template.close()  # discard memory
