09e94baa4698aab1d6bab5e51ca0d9d84d2fb070
Grant Ramsay <seapagan@gmail.com>
2024-09-01 11:16:35 +0100

    api: add the 'get_license' method and related tests
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lice2/api/api.py
+++ lice2/api/api.py
@@ -1,17 +1,30 @@
 """This defines an API that other Python code can use to interact with LICE2."""
 
+from lice2.api.exceptions import LanguageNotFoundError, LicenseNotFoundError
 from lice2.constants import LANGS, LICENSES
+from lice2.helpers import (
+    format_license,
+    generate_license,
+    get_local_year,
+    load_package_template,
+)
 
 
 class Lice:
     """List or Generate a License from many supported licenses."""
 
-    def __init__(self, organization: str, project: str) -> None:
+    def __init__(
+        self,
+        organization: str,
+        project: str,
+        year: str = get_local_year(),
+    ) -> None:
         """Initialize the Lice object.
 
         Args:
             organization: The name of the organization that owns the project.
             project: The name of the project.
+            year: The year to use in the license. Defaults to the current year.
 
         Note that not all licenses will use the 'project' field.
 
@@ -20,6 +33,7 @@ class Lice:
         """
         self.organization = organization
         self.project = project
+        self.year = year
 
     def get_licenses(self) -> list[str]:
         """Return a list of all licenses in the system.
@@ -48,3 +62,32 @@ class Lice:
             ['py', 'js', 'c', 'cpp', 'java', 'rs', 'rb', 'sh', 'html', ...]
         """
         return list(LANGS.keys())
+
+    def get_license(self, license_name: str, language: str = "") -> str:
+        """Return the text of the given license.
+
+        Args:
+            license_name: The name of the license to retrieve.
+            language: [OPTIONAL] If set, comment the license for that language.
+
+        Examples:
+            >>> lice = Lice(organization="Awesome Co.", project="my_project")
+            >>> licence_txt = Lice.get_license("mit")
+        """
+        args = {
+            "year": self.year,
+            "organization": self.organization,
+            "project": self.project,
+        }
+        try:
+            template = load_package_template(license_name)
+        except FileNotFoundError:
+            raise LicenseNotFoundError(license_name) from None
+
+        content = generate_license(template, args)
+
+        try:
+            out = format_license(content, language)
+        except KeyError:
+            raise LanguageNotFoundError(language) from None
+        return out.getvalue()
--- lice2/tests/test_api.py
+++ lice2/tests/test_api.py
@@ -9,6 +9,7 @@ from lice2.api.exceptions import (
     LicenseNotFoundError,
 )
 from lice2.constants import LANGS, LICENSES
+from lice2.helpers import get_local_year
 
 
 @pytest.fixture
@@ -20,72 +21,103 @@ def lice() -> Lice:
     )
 
 
-def test_lice_instance(lice: Lice) -> None:
-    """Test that the Lice instance is created correctly."""
-    assert isinstance(lice, Lice)
-    assert lice.organization == "Awesome Co."
-    assert lice.project == "my_project"
+class TestAPI:
+    """A test class for the API."""
 
+    def test_lice_instance(self, lice: Lice) -> None:
+        """Test that the Lice instance is created correctly."""
+        assert isinstance(lice, Lice)
+        assert lice.organization == "Awesome Co."
+        assert lice.project == "my_project"
 
-def test_license_not_found_error_no_arg() -> None:
-    """Test that raising LicenseNotFoundError without an argument fails."""
-    with pytest.raises(TypeError):
-        raise LicenseNotFoundError  # type: ignore[call-arg]
+    def test_license_not_found_error_no_arg(self) -> None:
+        """Test that raising LicenseNotFoundError without an argument fails."""
+        with pytest.raises(TypeError):
+            raise LicenseNotFoundError  # type: ignore[call-arg]
 
+    def test_language_not_found_error_no_arg(self) -> None:
+        """Test that raising LanguageNotFoundError without an argument fails."""
+        with pytest.raises(TypeError):
+            raise LanguageNotFoundError  # type: ignore[call-arg]
 
-def test_language_not_found_error_no_arg() -> None:
-    """Test that raising LanguageNotFoundError without an argument fails."""
-    with pytest.raises(TypeError):
-        raise LanguageNotFoundError  # type: ignore[call-arg]
+    def test_header_not_found_error_no_arg(self) -> None:
+        """Test that raising HeaderNotFoundError without an argument fails."""
+        with pytest.raises(TypeError):
+            raise HeaderNotFoundError  # type: ignore[call-arg]
 
+    def test_license_not_found_error(self) -> None:
+        """Test that LicenseNotFoundError is raised correctly."""
+        bad_license = "unknown_license"
+        with pytest.raises(LicenseNotFoundError) as exc_info:
+            raise LicenseNotFoundError(bad_license)
+        assert str(exc_info.value) == "License 'unknown_license' is unknown."
+        assert exc_info.value.license_name == "unknown_license"
 
-def test_header_not_found_error_no_arg() -> None:
-    """Test that raising HeaderNotFoundError without an argument fails."""
-    with pytest.raises(TypeError):
-        raise HeaderNotFoundError  # type: ignore[call-arg]
+    def test_language_not_found_error(self) -> None:
+        """Test that LanguageNotFoundError is raised correctly."""
+        bad_language = "unknown_language"
+        with pytest.raises(LanguageNotFoundError) as exc_info:
+            raise LanguageNotFoundError(bad_language)
+        assert str(exc_info.value) == "Language 'unknown_language' is unknown."
+        assert exc_info.value.language_name == "unknown_language"
 
+    def test_header_not_found_error(self) -> None:
+        """Test that HeaderNotFoundError is raised correctly."""
+        license_name = "unknown_license"
+        with pytest.raises(HeaderNotFoundError) as exc_info:
+            raise HeaderNotFoundError(license_name)
+        assert (
+            str(exc_info.value)
+            == "License 'unknown_license' does not have any headers."
+        )
+        assert exc_info.value.license_name == "unknown_license"
 
-def test_license_not_found_error() -> None:
-    """Test that LicenseNotFoundError is raised correctly."""
-    bad_license = "unknown_license"
-    with pytest.raises(LicenseNotFoundError) as exc_info:
-        raise LicenseNotFoundError(bad_license)
-    assert str(exc_info.value) == "License 'unknown_license' is unknown."
-    assert exc_info.value.license_name == "unknown_license"
+    def test_get_licenses(self, lice: Lice) -> None:
+        """Test that get_licenses returns a list of licenses."""
+        licenses = lice.get_licenses()
+        assert isinstance(licenses, list)
+        assert len(licenses) == len(LICENSES)
+        assert all(isinstance(license_name, str) for license_name in licenses)
 
+    def test_get_languages(self, lice: Lice) -> None:
+        """Test that get_languages returns a list of languages."""
+        languages = lice.get_languages()
+        assert isinstance(languages, list)
+        assert len(languages) == len(LANGS.keys())
+        assert all(isinstance(language, str) for language in languages)
 
-def test_language_not_found_error() -> None:
-    """Test that LanguageNotFoundError is raised correctly."""
-    bad_language = "unknown_language"
-    with pytest.raises(LanguageNotFoundError) as exc_info:
-        raise LanguageNotFoundError(bad_language)
-    assert str(exc_info.value) == "Language 'unknown_language' is unknown."
-    assert exc_info.value.language_name == "unknown_language"
+    def test_get_license(self, lice: Lice) -> None:
+        """Test we can get a standard license.
 
+        We'll use the AFL3 license as it uses all 3 context vars.
+        """
+        license_txt = lice.get_license("afl3")
 
-def test_header_not_found_error() -> None:
-    """Test that HeaderNotFoundError is raised correctly."""
-    license_name = "unknown_license"
-    with pytest.raises(HeaderNotFoundError) as exc_info:
-        raise HeaderNotFoundError(license_name)
-    assert (
-        str(exc_info.value)
-        == "License 'unknown_license' does not have any headers."
-    )
-    assert exc_info.value.license_name == "unknown_license"
+        this_year = get_local_year()
 
+        assert "Academic Free License" in license_txt
+        assert "Awesome Co." in license_txt
+        assert "my_project" in license_txt
+        assert str(this_year) in license_txt
 
-def test_get_licenses(lice: Lice) -> None:
-    """Test that get_licenses returns a list of licenses."""
-    licenses = lice.get_licenses()
-    assert isinstance(licenses, list)
-    assert len(licenses) == len(LICENSES)
-    assert all(isinstance(license_name, str) for license_name in licenses)
+    def test_get_license_unknown(self, lice: Lice) -> None:
+        """Test that get_license raises an exception for unknown licenses."""
+        with pytest.raises(LicenseNotFoundError) as exc_info:
+            lice.get_license("unknown_license")
+        assert str(exc_info.value) == "License 'unknown_license' is unknown."
+        assert exc_info.value.license_name == "unknown_license"
 
+    def test_get_license_language(self, lice: Lice) -> None:
+        """Test we can get a license for a specific language."""
+        license_txt = lice.get_license("mit", language="py")
 
-def test_get_languages(lice: Lice) -> None:
-    """Test that get_languages returns a list of languages."""
-    languages = lice.get_languages()
-    assert isinstance(languages, list)
-    assert len(languages) == len(LANGS.keys())
-    assert all(isinstance(language, str) for language in languages)
+        assert "The MIT License (MIT)" in license_txt
+        for line in license_txt.splitlines():
+            assert line.startswith("#")
+
+    def test_get_license_language_unknown(self, lice: Lice) -> None:
+        """Test that get_license raises an exception for unknown languages."""
+        with pytest.raises(LanguageNotFoundError) as exc_info:
+            lice.get_license("mit", language="unknown_language")
+        assert str(exc_info.value) == "Language 'unknown_language' is unknown."
+        assert exc_info.value.language_name == "unknown_language"
